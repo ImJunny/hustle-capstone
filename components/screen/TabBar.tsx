249ea9c702cbff6cbe5ce5fd20c1942f1605c8e7
John Nguyen <johnnguyen4501@gmail.com>
2025-02-02 00:49:56 -0500

    remove unused variable

--- components/screen/TabBar.tsx
+++ components/screen/TabBar.tsx
@@ -24,12 +24,6 @@ export default function TabBar({
     >
       {state.routes.map((route: any, index: any) => {
         const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
 
         const isFocused = state.index === index;
 

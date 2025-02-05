b12603d2f7be576114738f5a7b427c458b4de512
DanmDza <dzamdaniel@gmail.com>
2025-02-04 21:50:52 -0500

    Created a CategoryCard component under explore in components. Added the top category section with a horizontal scrollview which allows users to swipe through different categories.

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,12 +3,41 @@ import View from "@/components/ui/View";
 import React from "react";
 import { router } from "expo-router";
 import Button from "@/components/ui/Button";
+import ScrollView from "@/components/ui/ScrollView";
+import CategoryComponent from "@/components/explore/CategoryCard";
+import ImageBackgroundPlaceholder from "@/components/ui/ImageBackgroundPlaceholder";
+import Separator from "@/components/ui/Separator";
+import CategoryCard from "@/components/explore/CategoryCard";
+import { StyleSheet } from "react-native";
 
+const categories = [
+  { id: 1, title: "Technology" },
+  { id: 2, title: "Health" },
+  { id: 3, title: "Finance" },
+  { id: 4, title: "Education" },
+];
 export default function ExploreScreen() {
   return (
-    <View>
-      <Text>Explore page/Search page!</Text>
-      <Button onPress={() => router.push("/job-post")}>~Go to a job post~</Button>
-    </View>
+    <ScrollView style={styles.container} color="background">
+      <View>
+        <Text size="xl" weight="bold" color="white" style={{ marginBottom: 8 }}>
+          Top Categories
+        </Text>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          {categories.map((category, i) => (
+            <CategoryCard key={category.id} title={category.title} style={{}} />
+          ))}
+        </ScrollView>
+        <Button onPress={() => router.push("/job-post")}>
+          ~Go to a job post~
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+});
---
+++ components/explore/CategoryCard.tsx
@@ -0,0 +1,25 @@
+import View, { ViewProps } from "@/components/ui/View";
+import Text from "@/components/ui/Text";
+import ImageBackgroundPlaceholder from "@/components/ui/ImageBackgroundPlaceholder";
+import { StyleSheet } from "react-native";
+import React from "react";
+
+type CategoryCardProps = {
+  title: string;
+} & ViewProps;
+
+export default function CategoryCard({ title, style }: CategoryCardProps) {
+  return (
+    <View style={[{ marginRight: 10 }, style]}>
+      <ImageBackgroundPlaceholder
+        width={100}
+        height={100}
+        style={{ justifyContent: "center" }}
+      >
+        <Text style={{ textAlign: "center" }}>{title}</Text>
+      </ImageBackgroundPlaceholder>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});

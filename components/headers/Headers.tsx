9c4161fe33dffece695032ca3791a224f1e218e2
John Nguyen <johnnguyen4501@gmail.com>
2025-02-06 03:44:46 -0500

    create new header file

---
+++ components/headers/Headers.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import HeaderWrapper from "./HeaderWrapper";
+import Text from "../ui/Text";
+import IconButton from "../ui/IconButton";
+import { router } from "expo-router";
+
+export function ExampleHeader() {
+  return (
+    <HeaderWrapper
+      options={{
+        left: (
+          <Text
+            weight="semibold"
+            size="2xl"
+            style={{
+              fontFamily: "Lexend-bold",
+            }}
+          >
+            Hustle
+          </Text>
+        ),
+        center: <Text>Middle</Text>,
+        right: (
+          <IconButton
+            name="information-circle-outline"
+            size="xl"
+            onPress={() => router.push("/example")}
+          />
+        ),
+      }}
+    />
+  );
+}
+
+export function JobsHeader() {
+  return (
+    <HeaderWrapper
+      options={{
+        left: (
+          <Text weight="semibold" size="xl">
+            Job Center
+          </Text>
+        ),
+        right: (
+          <IconButton
+            name="notifications-outline"
+            size="xl"
+            onPress={() => router.push("/example")}
+          />
+        ),
+      }}
+    />
+  );
+}
+
+export function BackHeader() {
+  return (
+    <HeaderWrapper
+      options={{
+        left: (
+          <IconButton
+            name="arrow-back"
+            size="xl"
+            onPress={() => router.back()}
+          />
+        ),
+      }}
+    />
+  );
+}
+
+export function EmptyHeader() {
+  return <HeaderWrapper />;
+}

0166dad8620286aadf2b7cf4982b1f9a50f09203
DanmDza <99596868+DanmDza@users.noreply.github.com>
2025-02-13 01:57:09 -0500

    Created profile and settings page (#21)

--- app/(main)/(tabs)/profile-main.tsx
+++ app/(main)/(tabs)/profile-main.tsx
@@ -2,16 +2,34 @@ import Text from "@/components/ui/Text";
 import View from "@/components/ui/View";
 import React from "react";
 import { router } from "expo-router";
-import Button from "@/components/ui/Button";
-import { EmptyHeader } from "@/components/headers/Headers";
+import { ProfileHeader } from "@/components/headers/Headers";
+import ProfileBio from "@/components/profile/profileBio";
 
 export default function ProfileMainScreen() {
   return (
     <>
-      <EmptyHeader />
+      <ProfileHeader />
       <View>
-        <Text>Profile Main</Text>
-        <Button onPress={() => router.push("/settings")}>Settings123</Button>
+        <ProfileBio
+          data={{
+            uuid: "",
+            bio: "",
+            user_name: "John smith",
+            name: "",
+          }}
+        />
+        <View>
+          <Text
+            color="muted"
+            style={{
+              paddingHorizontal: 15,
+            }}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation...
+          </Text>
+        </View>
       </View>
     </>
   );

8eb890ec1cb19d39eeb209991391153a64858443
DanmDza <dzamdaniel@gmail.com>
2025-02-16 19:38:54 -0500

    review component

--- app/(main)/(external)/service-reviews/[id].tsx
+++ app/(main)/(external)/service-reviews/[id].tsx
@@ -1,128 +1,13 @@
 import Text from "@/components/ui/Text";
 import View from "@/components/ui/View";
 import React from "react";
-import { router, useLocalSearchParams } from "expo-router";
-import { ReviewHeader } from "@/components/headers/Headers";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import Button from "@/components/ui/Button";
-import Icon from "@/components/ui/Icon";
-import IconButton from "@/components/ui/IconButton";
-import { exampleReview } from "@/server/utils/example_data";
-import { useThemeColor } from "@/hooks/useThemeColor";
-import { useAuthData } from "@/contexts/AuthContext";
-import { useQuery } from "@tanstack/react-query";
-import LoadingScreen from "@/components/ui/LoadingScreen";
-import { getUserData } from "@/server/lib/user";
-import ImagePlaceholder from "@/components/ui/ImagePlaceholder";
-import { LinearGradient } from "expo-linear-gradient";
+import Review from "@/components/ui/Review";
 
 export default function ReviewScreen() {
-  const { id } = useLocalSearchParams();
-  const review = exampleReview.find((review) => review.uuid === id);
-  const themeColor = useThemeColor();
-  const { user } = useAuthData();
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["userDataQuery", user],
-    queryFn: () => getUserData(user?.id!),
-  });
-
-  if (error) {
-    return (
-      <View>
-        <Text>User not found.</Text>
-      </View>
-    );
-  }
-  console.log(user);
-  if (isLoading) {
-    return <LoadingScreen />;
-  }
-
-  if (!review) {
-    return <Text>review not found</Text>;
-  }
   return (
-    <>
-      <ReviewHeader username={data?.username ?? ""} />
-      <View>
-        <ImagePlaceholder width={800} height={320} />
-        <LinearGradient
-          colors={["rgb(0, 0, 0)", "transparent"]}
-          start={{ x: 0, y: 1 }}
-          end={{ x: 0, y: 0 }}
-          style={{
-            left: 0,
-            right: 0,
-            height: 250,
-            bottom: -2,
-            position: "absolute",
-          }}
-        />
-      </View>
-      <View
-        style={{
-          padding: 15,
-          borderBottomColor: "grey",
-          borderBottomWidth: 2,
-        }}
-      >
-        <View style={styles.bottomContainer}>
-          <View
-            style={{
-              borderRadius: 999,
-              width: 50,
-              height: 50,
-            }}
-            color="muted"
-          />
-          <View style={styles.nameContainer}>
-            <Text color="white" weight="semibold">
-              {review.user_name} | {review.date}
-            </Text>
-
-            <View
-              style={{ flexDirection: "row", gap: 4, alignItems: "center" }}
-            >
-              <Icon name={"star"} color="white"></Icon>
-
-              <Text color="white" weight="semibold">
-                4.5/5
-              </Text>
-              <View style={styles.userTypeContainer}>
-                <Text color="muted" weight="semibold">
-                  Employer
-                </Text>
-              </View>
-            </View>
-          </View>
-        </View>
-
-        <View
-          style={{ flexDirection: "row", marginVertical: 15, marginBottom: 15 }}
-        >
-          <Text numberOfLines={15} ellipsizeMode="tail" color="muted-dark">
-            {review.description}
-          </Text>
-        </View>
-      </View>
-    </>
+    <View>
+      <Review />
+      <Text>ReviewScreen</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  bottomContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    marginTop: 8,
-  },
-  nameContainer: { marginLeft: 20 },
-  viewButton: {
-    marginLeft: "auto",
-    backgroundColor: "grey",
-    paddingHorizontal: 30,
-  },
-  userTypeContainer: {
-    marginLeft: 60,
-  },
-});
---
+++ components/ui/Review.tsx
@@ -0,0 +1,111 @@
+import Text from "@/components/ui/Text";
+import View from "@/components/ui/View";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+import { StyleSheet } from "react-native";
+import Icon from "@/components/ui/Icon";
+import { exampleReview } from "@/server/utils/example_data";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { useAuthData } from "@/contexts/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import LoadingScreen from "@/components/ui/LoadingScreen";
+import { getUserData } from "@/server/lib/user";
+import ImagePlaceholder from "@/components/ui/ImagePlaceholder";
+
+export default function Review() {
+  const { id } = useLocalSearchParams();
+  const review = exampleReview.find((review) => review.uuid === id);
+  const themeColor = useThemeColor();
+  const { user } = useAuthData();
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["userDataQuery", user],
+    queryFn: () => getUserData(user?.id!),
+  });
+
+  if (error) {
+    return (
+      <View>
+        <Text>User not found.</Text>
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (!review) {
+    return <Text>Review not found</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          padding: 15,
+          borderBottomColor: "grey",
+          borderBottomWidth: 2,
+        }}
+      >
+        <View style={styles.bottomContainer}>
+          <View style={styles.nameContainer}>
+            <Text color="white" weight="semibold">
+              {review.user_name} | {review.date}
+            </Text>
+
+            <View style={styles.starsContainer}>
+              <Icon name="star" />
+              <Icon name="star" />
+              <Icon name="star" />
+              <Icon name="star" />
+              <Icon name="star" />
+              <Text style={styles.ratingText}>4.5/5</Text>
+            </View>
+          </View>
+          <ImagePlaceholder width={116} height={116} style={styles.imgStyle} />
+        </View>
+
+        <View style={styles.descriptionContainer}>
+          <Text numberOfLines={15} ellipsizeMode="tail" color="muted-dark">
+            {review.description}
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: 8,
+  },
+  nameContainer: {
+    flex: 1,
+  },
+  starsContainer: {
+    flexDirection: "row",
+    gap: 2,
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  ratingText: {
+    marginHorizontal: 5,
+    color: "white",
+    fontWeight: "semibold",
+  },
+  imgStyle: {
+    borderRadius: 4,
+  },
+  descriptionContainer: {
+    flexDirection: "row",
+    marginVertical: 15,
+    marginBottom: 15,
+  },
+});

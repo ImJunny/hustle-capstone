c209bdbccf27845accc4d1269cbe6e0f533a242b
John Nguyen <johnnguyen4501@gmail.com>
2025-03-29 04:57:27 -0400

    Fix search header

--- components/headers/Headers.tsx
+++ components/headers/Headers.tsx
@@ -325,8 +325,7 @@ export function SearchingHeader() {
   const [value, setValue] = useState(text as string);
 
   async function handleSearch() {
-    if (value.length > 0) router.back();
-    router.replace(`/search/${value}`);
+    if (value.length > 0) router.replace(`/search/${value}`);
   }
   return (
     <HeaderWrapper

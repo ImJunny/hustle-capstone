58b7814bf0b525515cf5baa309a4d8e5a81126f4
John Nguyen <johnnguyen4501@gmail.com>
2025-04-03 04:49:15 -0400

    Add toast message

--- components/posts/PostDetails.tsx
+++ components/posts/PostDetails.tsx
@@ -47,7 +47,15 @@ export default function PostDetails({ uuid }: { uuid: string }) {
   const handleSaveToggle = useCallback(() => {
     const newIsSaved = !isSaved;
     newIsSaved ? savePost(uuid) : unsavePost(uuid);
-
+    newIsSaved
+      ? Toast.show({
+          text1: "Saved",
+          visibilityTime: 500,
+        })
+      : Toast.show({
+          text1: "Unsaved",
+          visibilityTime: 500,
+        });
     const mutation = newIsSaved ? saveMutation : unsaveMutation;
     mutation.mutate(
       { post_uuid: uuid, user_uuid: user?.id! },

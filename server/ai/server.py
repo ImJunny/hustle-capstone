298e018812fa9ac9b9385081ba8a2cc3ce80ce41
John Nguyen <johnnguyen4501@gmail.com>
2025-04-10 04:09:10 -0400

    Add python server file

---
+++ server/ai/server.py
@@ -0,0 +1,70 @@
+import re
+from nltk.corpus import stopwords # type: ignore
+from nltk.tokenize import word_tokenize # type: ignore
+from fastapi import FastAPI, Query # type: ignore
+from groq import Groq # type: ignore
+from dotenv import load_dotenv # type: ignore
+import os
+import nltk # type: ignore
+
+nltk.download('punkt')
+nltk.download('stopwords')
+load_dotenv()
+api_key = os.getenv("GROQ_SECRET_KEY")
+
+app = FastAPI()
+client = Groq(
+    api_key=api_key,
+)
+
+def detect_sensitive_info(tokens):
+  patterns = {
+    "phone_numbers": r"\b\d{3}[-.\s]??\d{3}[-.\s]??\d{4}\b",
+    "emails": r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b",
+    "ssns": r"\b\d{3}-\d{2}-\d{4}\b",
+    "addresses": r"\b\d{1,5}\s(?:[A-Za-z0-9#]+\s?)+,\s(?:[A-Za-z]+\s?)+,\s[A-Z]{2}\s\d{5}(-\d{4})?\b"  # Enhanced address pattern
+  }
+  
+  detected_info = {key: [] for key in patterns}
+  for token in tokens:
+    for key, pattern in patterns.items():
+      if re.search(pattern, token):
+        detected_info[key].append(token)
+  
+  return detected_info
+
+
+@app.get("/getIsSafe")
+def determineSafe(input_text: str = Query(..., description="Input string to minimize unnecessary words")):
+  # use NLTK
+  stop_words = set(stopwords.words('english'))
+  tokens = word_tokenize(input_text)
+  
+  sensitive_info = detect_sensitive_info(tokens)
+  is_safe = not any(sensitive_info[key] for key in sensitive_info)
+  if not sensitive_info:
+    return {
+      "response": is_safe,
+      "source" : "nltk"
+    }
+  
+  # use Groq
+  minimized_tokens = [token for token in tokens if token.lower() not in stop_words]
+  minimized_text = " ".join(minimized_tokens
+  )
+
+  chat_completion = client.chat.completions.create(
+      messages=[
+          {
+              "role": "user",
+              "content": minimized_text,
+          }
+      ],
+      model="gemma2-9b-it",
+  )
+
+  response = chat_completion.choices[0].message.content.strip()
+  return {
+    "response": response,
+    "source":"groq"
+  }

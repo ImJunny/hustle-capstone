6306083df6ed2791e65a7db990ac2af2213e0c0a
ImJunny <johnnguyen4501@gmail.com>
2025-04-13 13:44:00 -0400

    Remove unnecessary files

--- stripe-server.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-
-export const stripe = new Stripe(process.env.EXPO_PUBLIC_STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-02-24.acacia",
-  appInfo: {
-    name: "Hustle",
-  },
-});
